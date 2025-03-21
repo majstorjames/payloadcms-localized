e783fc5a17b5bd0ee1dd7f43e1751fb6b12f68f9
Jamey Y <jamey@Jameys-MacBook-Air.local>
2025-03-21 17:45:15 +0100

    Revert config back to original

--- next.config.js
+++ next.config.js
@@ -1,53 +1,24 @@
-// import { withPayload } from '@payloadcms/next/withPayload'
-// import createNextIntlPlugin from 'next-intl/plugin'
-
-// const withNextIntl = createNextIntlPlugin()
-
-// import redirects from './redirects.js'
-// import { hostname } from 'os'
-
-// const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
-//         const url = new URL(item)
-
-//         return {
-//           // hostname: url.hostname,
-//           // protocol: url.protocol.replace(':', ''),
-//           protocol: 'https',
-//           hostname: process.env.NEXT_CONFIG_HOSTNAME,
-//         }
-//       }),
-//     ],
-//   },
-//   reactStrictMode: true,
-//   redirects,
-// }
-
-// export default withNextIntl(withPayload(nextConfig))
-
-
 import { withPayload } from '@payloadcms/next/withPayload'
 import createNextIntlPlugin from 'next-intl/plugin'
 const withNextIntl = createNextIntlPlugin()
-
 import redirects from './redirects.js'
 
-const SUPABASE_HOSTNAME = process.env.NEXT_CONFIG_HOSTNAME || 'oppmryradcadimfmehub.supabase.co'
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: SUPABASE_HOSTNAME,
-        pathname: '/storage/v1/object/public/**',
-      },
+      ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
+        const url = new URL(item)
+
+        return {
+          // hostname: url.hostname,
+          // protocol: url.protocol.replace(':', ''),
+          protocol: 'https',
+          hostname: process.env.NEXT_CONFIG_HOSTNAME,
+        }
+      }),
     ],
   },
   reactStrictMode: true,
@@ -55,3 +26,29 @@ const nextConfig = {
 }
 
 export default withNextIntl(withPayload(nextConfig))
+
+
+// import { withPayload } from '@payloadcms/next/withPayload'
+// import createNextIntlPlugin from 'next-intl/plugin'
+// const withNextIntl = createNextIntlPlugin()
+
+// import redirects from './redirects.js'
+
+// const SUPABASE_HOSTNAME = process.env.NEXT_CONFIG_HOSTNAME || 'oppmryradcadimfmehub.supabase.co'
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: SUPABASE_HOSTNAME,
+//         pathname: '/storage/v1/object/public/**',
+//       },
+//     ],
+//   },
+//   reactStrictMode: true,
+//   redirects,
+// }
+
+// export default withNextIntl(withPayload(nextConfig))
